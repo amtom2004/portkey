67fb1b4e7ba49805e0945c2306cb665934f4c9d6
amtom2004 <amtombcs@gmail.com>
2025-10-07 19:55:57 +0530

    INIT-001 - Initisl Commit

---
+++ src/main/java/com/phileo/portkey/PortkeyApplication.java
@@ -0,0 +1,13 @@
+package com.phileo.portkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortkeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortkeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phileo/portkey/PortkeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phileo.portkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortkeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
